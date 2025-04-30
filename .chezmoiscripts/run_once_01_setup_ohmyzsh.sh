9861f92f2446b4060f65b8c2d9241e1d63b58e84
tasogare-88 <tasogare.dev.88@gmail.com>
2025-04-30 13:39:21 +0900

    update: ⤴️ 既存のohmyzshの削除

--- .chezmoiscripts/run_once_01_setup_ohmyzsh.sh
+++ .chezmoiscripts/run_once_01_setup_ohmyzsh.sh
@@ -17,13 +17,29 @@ if [ ! -f /etc/zshenv ]; then
 fi
 echo "ZDOTDIR=\$HOME/.config/zsh" | sudo tee /etc/zshenv
 
+# 既存のoh-my-zshディレクトリを削除
+if [ -d "$HOME/.oh-my-zsh" ]; then
+    echo "Removing existing oh-my-zsh installation..."
+    rm -rf "$HOME/.oh-my-zsh"
+fi
+
 echo "Installing oh-my-zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# ZSH環境変数をクリアしてインストール
+ZSH= sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 echo "Installing oh-my-zsh plugins..."
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
-git clone https://github.com/fdellwing/zsh-bat.git $ZSH_CUSTOM/plugins/zsh-bat
-git clone --recurse-submodules https://github.com/olets/zsh-abbr.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-abbr
+# プラグインディレクトリが存在する場合は削除
+for plugin in zsh-completions zsh-autosuggestions zsh-syntax-highlighting you-should-use zsh-bat zsh-abbr; do
+    plugin_dir="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/$plugin"
+    if [ -d "$plugin_dir" ]; then
+        rm -rf "$plugin_dir"
+    fi
+done
+
+# プラグインのインストール
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/you-should-use
+git clone https://github.com/fdellwing/zsh-bat.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-bat
+git clone --recurse-submodules https://github.com/olets/zsh-abbr.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-abbr
